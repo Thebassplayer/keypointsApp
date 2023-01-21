82b69d598a38c4691ef5fbc628c8296e23e09baa
Roy Lopez <windup.dues-0f@icloud.com>
2023-01-21 20:23:21 -0300

    fix env bug

--- src/client.js
+++ src/client.js
@@ -2,11 +2,11 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  projectId: `${import.meta.env.VITE_SANITY_PROJECT_ID}`,
   dataset: "production",
   apiVersion: "2022-12-27",
   useCdn: true,
-  token: import.meta.env.VITE_SANITY_TOKEN,
+  token: `${import.meta.env.VITE_SANITY_TOKEN}`,
 });
 
 const builder = imageUrlBuilder(client);
