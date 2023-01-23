1d4634c8f345c3f29fa881b65584792665206dcd
Roy Lopez <windup.dues-0f@icloud.com>
2023-01-23 16:39:03 -0300

    spinner

--- src/components/ExampleCode.component.jsx
+++ src/components/ExampleCode.component.jsx
@@ -5,7 +5,6 @@ import embedProject from "../utils/stackblitz/embedCode.utils";
 function ExampleCode({ example, id }) {
   if (example) {
     useEffect(() => {
-      console.log("id: ", id);
       embedProject(example, id);
     }, []);
   }
