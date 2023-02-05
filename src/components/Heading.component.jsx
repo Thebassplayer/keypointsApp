ac549512157d9123c133de40bda05b27fe35b31e
Roy Lopez <windup.dues-0f@icloud.com>
2023-02-05 09:06:18 -0300

    small resolution heading fix

--- src/components/Heading.component.jsx
+++ src/components/Heading.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Heading() {
   return (
     <div className="custom-heading-div container mb-8 w-11/12 p-4 text-center font-VT323 text-6xl font-bold drop-shadow-2xl">
-      <h1 className="m-0 p-0 text-3xl text-black shadow-white drop-shadow-lg sm:text-4xl md:text-5xl">
+      <h1 className="m-0 p-0 text-2xl text-black shadow-white drop-shadow-lg sm:text-4xl md:text-5xl">
         Javascript Keypoints App
       </h1>
     </div>
