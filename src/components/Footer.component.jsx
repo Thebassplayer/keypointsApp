64f4899baec681ff39be871c361b7c6cbc16c837
Roy Lopez <windup.dues-0f@icloud.com>
2023-02-05 10:42:23 -0300

    content table height fix

--- src/components/Footer.component.jsx
+++ src/components/Footer.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="w-full pt-4 text-center">
+    <div className="w-full pt-4 text-center text-xs">
       by <a href="https://roylopez.dev/">Roy Lopez - All rights reserved</a>
     </div>
   );
--- src/components/Heading.component.jsx
+++ src/components/Heading.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Heading() {
   return (
-    <div className="custom-heading-div container mb-8 w-11/12 p-4 text-center font-VT323  font-bold">
+    <div className="custom-heading-div container mb-4 w-11/12 p-4 text-center font-VT323  font-bold">
       <h1 className="m-0 p-0 text-2xl text-black shadow-white drop-shadow-lg sm:text-4xl md:text-5xl">
         Javascript Keypoints App
       </h1>
