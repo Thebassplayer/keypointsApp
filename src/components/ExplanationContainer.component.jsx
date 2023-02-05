4d8d1fcffda39bb6bac19a31904cde60ee6c57ac
Roy Lopez <windup.dues-0f@icloud.com>
2023-02-04 21:38:11 -0300

    apply tailwind styles

--- src/components/ExplanationContainer.component.jsx
+++ src/components/ExplanationContainer.component.jsx
@@ -8,7 +8,7 @@ function ExplanationContainer({ props }) {
   const { _id, title, explanation, example } = props;
   return (
     <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-      <div className="prose">
+      <div className="prose max-w-none pb-2">
         <PortableText value={explanation} />
       </div>
       {example ? <ExampleCodeContainer example={example} /> : ""}
