4ee799e8e99a427895d43aa446f838e739510950
Roy Lopez <windup.dues-0f@icloud.com>
2023-02-05 20:05:24 -0300

    fix tipo

--- src/components/Footer.component.jsx
+++ src/components/Footer.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="w-full py-1 text-center text-xs">
-      by <a href="https://roylopez.dev/">Roy Lopez - All rights reserved</a>
+      by <a href="https://roylopez.dev/">Roy Lopez</a> - All rights reserved
     </div>
   );
 }
