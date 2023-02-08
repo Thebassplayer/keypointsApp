4272f7466d2e80c09aa2963dd8c20de538dd0ace
Roy Lopez <windup.dues-0f@icloud.com>
2023-02-08 08:50:43 -0300

    apply styles to pre codeblocks

--- src/components/ExplanationContainer.component.jsx
+++ src/components/ExplanationContainer.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { PortableText } from "@portabletext/react";
+import SanityCodehighlighted from "./SanityCodeHighlighted/SanityCodeHighlighted.component";
 
 import { Disclosure } from "@headlessui/react";
 import ExampleCodeContainer from "./ExampleCodeContainer.component";
@@ -7,11 +8,19 @@ import StackBlitzCode from "./StackblitzCode.component";
 function ExplanationContainer({ props }) {
   const { _id, title, explanation, example } = props;
 
+  const sanityCode = ({ value }) =>
+    value && <SanityCodehighlighted language="js" code={value.code} />;
+  const sanityComponents = {
+    types: {
+      code: sanityCode,
+    },
+  };
+
   return (
     <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
       {explanation && (
         <div className="prose max-w-none pb-2">
-          <PortableText value={explanation} />
+          <PortableText value={explanation} components={sanityComponents} />
         </div>
       )}
       {example ? <ExampleCodeContainer example={example} /> : ""}
