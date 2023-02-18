c2742fdd1be3c872bc055019d0a474bc8a6930c9
Roy Lopez <windup.dues-0f@icloud.com>
2023-02-18 09:06:16 -0300

    clean unused hooks

--- src/components/ConceptsTable.component.jsx
+++ src/components/ConceptsTable.component.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import { useSanityData } from "../contexts/SanityConceptsContext";
 import { useTag } from "../contexts/selectedTagsContext";
--- src/components/SearchBar.component.jsx
+++ src/components/SearchBar.component.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 function SearchBar({ setSearchValue, searchValue }) {
   return (
