d50a4c9fb5cb781b972ef5e6cb478e3b39cc715d
Roy Lopez <roylopezdev@gmail.com>
2023-03-08 08:30:27 -0300

    remove unused files

--- src/hooks/useDebouncedSearch.tsx
+++ src/hooks/useDebouncedSearch.tsx
@@ -13,15 +13,32 @@
 
 import { useState, useEffect, useRef } from "react";
 
-export const useDebouncedSearch = (
+interface UseDebouncedSearchParams {
+  concepts: Concept[];
+  searchValue: string;
+  tags: string[];
+  selectedTag: string;
+  delay: number;
+}
+
+interface Concept {
+  title: string;
+  tags: string[];
+}
+
+interface UseDebouncedSearchResult {
+  searchResults: Concept[];
+}
+
+const useDebouncedSearch = ({
   concepts,
   searchValue,
   tags,
   selectedTag,
-  delay
-) => {
-  const [searchResults, setSearchResults] = useState([]);
-  const timeoutId = useRef(null);
+  delay,
+}: UseDebouncedSearchParams): UseDebouncedSearchResult => {
+  const [searchResults, setSearchResults] = useState<Concept[]>([]);
+  const timeoutId = useRef<number | null>(null);
 
   // Tag filter
   useEffect(() => {
@@ -35,7 +52,7 @@ export const useDebouncedSearch = (
       !selectedTag ? true : tags && tags.includes(selectedTag)
     );
     setSearchResults(resultsFilteredByTag);
-  }, [tags, selectedTag]);
+  }, [concepts, selectedTag]);
 
   // Search filter
   useEffect(() => {
@@ -46,7 +63,7 @@ export const useDebouncedSearch = (
     }
 
     // Update debounced value after delay
-    timeoutId.current = setTimeout(() => {
+    timeoutId.current = window.setTimeout(() => {
       const resultsFilteredBySearch = concepts.filter(
         ({ title }) =>
           title && title.toLowerCase().includes(searchValue.toLowerCase())
@@ -58,9 +75,11 @@ export const useDebouncedSearch = (
     // This is how we prevent debounced value from updating if value is changed ...
     // .. within the delay period. Timeout gets cleared and restarted.
     return () => {
-      clearTimeout(timeoutId.current);
+      if (timeoutId.current) {
+        clearTimeout(timeoutId.current);
+      }
     };
-  }, [concepts, searchValue, delay]);
+  }, [concepts, delay, searchValue]);
 
   return { searchResults };
 };
